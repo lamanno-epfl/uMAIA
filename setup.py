5809bd89b8560e9032c2602b4a31fc550fa0c828
alireza7agm <alireza.agm@gmail.com>
2023-09-11 18:27:13 +0200

    tutorial updated.

--- setup.py
+++ setup.py
@@ -5,19 +5,17 @@ import os
 
 # Attempt to create a Conda environment from a .yml file
 try:
-    subprocess.run(["conda", "env", "create", "-f", "w_env.yml", "--name", "w_env"])
-    subprocess.run(["conda", "run", "-n", "w_env", "pip", "install", "--force-reinstall", "jax[cuda12_pip]", "-f", "https://storage.googleapis.com/jax-releases/jax_cuda_releases.html"], check=True)
+    subprocess.run(["conda", "env", "create", "-f", "environment.yml", "--name", "myenv"])
+    subprocess.run(["conda", "run", "-n", "myenv", "pip", "install", "--force-reinstall", "jax[cuda12_pip]", "-f", "https://storage.googleapis.com/jax-releases/jax_cuda_releases.html"], check=True)
 except subprocess.CalledProcessError:
     print("Failed to create conda environment from environment.yml.")
     sys.exit(1)
     
     
-
 # Attempt to install a specific package via pip (force install)
 try:
-    subprocess.run(["conda", "run", "-n", "w_env", "pip", "install", "--force-reinstall", "numba==0.57.1"], check=True)
-    subprocess.run(["conda", "run", "-n", "w_env", "pip", "install", "--force-reinstall", "numpy==1.23.0"], check=True)
-    #subprocess.run(["conda", "run", "-n", "MYENV2", "pip", "install", "--force-reinstall", "numba"], check=True)
+    subprocess.run(["conda", "run", "-n", "myenv", "pip", "install", "--force-reinstall", "numba==0.57.1"], check=True)
+    subprocess.run(["conda", "run", "-n", "myenv", "pip", "install", "--force-reinstall", "numpy==1.23.0"], check=True)
 except subprocess.CalledProcessError:
     print("Failed to force-install the pip package.")
     sys.exit(1)
@@ -25,9 +23,12 @@ except subprocess.CalledProcessError:
 os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "false"
 
 setup(
-    name='YourPackageName',
+    name='MAIA',
     version='0.1',
     packages=find_packages(),
+    description='Python Distribution Utilities',
+    author='@lamanno-epfl',
+    author_email='gioele.lamanno@epfl.ch',
     install_requires=[
         # Your pip package requirements here
     ],
