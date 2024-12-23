c53e53c7aadb810f28e2633f18a328bc68609004
hhschede <halima.schede@epfl.ch>
2024-12-23 18:15:52 +0100

    gamma distribution for delta prior

--- uMAIA/normalization/_model.py
+++ uMAIA/normalization/_model.py
@@ -11,7 +11,8 @@ from numpyro.infer import SVI, TraceEnum_ELBO, init_to_value
 from numpyro.infer.autoguide import AutoDelta
 
 @config_enumerate
-def model(data, mask, covariate_vector=None, D_matrix_ones=None, priors_hyperparameters=None, flex_mean=0.5):
+def model(data, mask, covariate_vector=None, D_matrix_ones=None, 
+          priors_hyperparameters=None, delta_v_dist='gaussian',flex_mean=0.5):
     N, S, V = data.shape
     K = 2
 
@@ -45,10 +46,16 @@ def model(data, mask, covariate_vector=None, D_matrix_ones=None, priors_hyperpar
         
       
     with V_plate:
-        delta_v = numpyro.sample('delta_v', 
-                                dist.Normal(jnp.full((V), 3.0) ,
-                                            jnp.full((V), 0.8))
-                              ) 
+        if delta_v_dist=='gamma':
+            delta_v = numpyro.sample('delta_v', 
+                                    dist.Normal(jnp.full((V), 3.0) ,
+                                                jnp.full((V), 0.8))
+                                  )
+        else:
+            delta_v = numpyro.sample('delta_v', 
+                                    dist.Gamma(jnp.full((V), 5.0) ,
+                                                jnp.full((V), 2.0))
+                                  ) #dist.Gamma(3, 0.8)
         with C_plate:
             # delta = numpyro.sample('delta', 
             #                     dist.Normal(jnp.full((n_cov,1, 1, V), 3.0) ,
@@ -56,7 +63,7 @@ def model(data, mask, covariate_vector=None, D_matrix_ones=None, priors_hyperpar
             #                   ) 
             delta = numpyro.sample('delta', 
                                 dist.Normal(jnp.full((n_cov,1, 1, V), delta_v) ,
-                                            jnp.full((n_cov,1, 1, V), 0.1))
+                                            jnp.full((n_cov,1, 1, V), 0.25))
                               ) 
 
          
@@ -89,6 +96,109 @@ def model(data, mask, covariate_vector=None, D_matrix_ones=None, priors_hyperpar
     
             batch_term = b_lambda*b_gamma
             batch_term = batch_term + batch_error
+            
+    with (S_plate, V_plate, D_plate):
+        with handlers.mask(mask = mask):
+            assignment = numpyro.sample('assignment', dist.Categorical(weights),
+                                   # infer={"enumerate":"parallel"}
+                                )
+
+            obs = numpyro.sample('obs', dist.Normal(jnp.where(assignment==0, 
+                                                        locs, 
+                                                        locs + delta_ + batch_term), 
+                                                    jnp.where(assignment==0,
+                                                        scale1, 
+                                                        sigma_s + sigma_v)),
+                    obs=data) 
+
+@config_enumerate
+def model_rank2(data, mask, covariate_vector=None, D_matrix_ones=None, priors_hyperparameters=None, flex_mean=0.5):
+    N, S, V = data.shape
+    K = 2
+
+    if covariate_vector == None:
+        covariate_vector = np.zeros((S,), dtype=np.int8)
+    n_cov = len(np.unique(covariate_vector))
+    covariate_vector = jnp.array(covariate_vector)
+
+    # D_matrix construction
+    if D_matrix_ones == None:
+        D_matrix_ones = jnp.zeros((n_cov,S))
+        for i, c in enumerate(covariate_vector):
+            D_matrix_ones = D_matrix_ones.at[c, i].set(1)
+    else:
+        n_cov = D_matrix_ones.shape[0]
+    D_matrix_ones_unsqueeze = jnp.expand_dims(D_matrix_ones, axis=(2,3))
+
+    # The Model
+    V_plate = numpyro.plate('v_plate', V,dim=-1)
+    S_plate = numpyro.plate('s_plate', S, dim=-2)
+    D_plate = numpyro.plate('d_plate', len(data), dim=-3)
+    C_plate = numpyro.plate('c_plate', n_cov, dim=-4)
+    
+    with S_plate:
+        b_gamma = numpyro.sample('b_gamma', dist.Uniform(jnp.full((S, 1), -2),
+                                                       jnp.full((S, 1), 2))
+        )
+        b_gamma2 = numpyro.sample('b_gamma2', dist.Uniform(jnp.full((S, 1), -2),
+                                                       jnp.full((S, 1), 2))
+        )
+
+        sigma_s = numpyro.sample('sigma_s', dist.Exponential(jnp.full((S, 1), 
+                                                                      priors_hyperparameters['rate0_sigma_s'])))
+        
+      
+    with V_plate:
+        delta_v = numpyro.sample('delta_v', 
+                                dist.Normal(jnp.full((V), 3.0) ,
+                                            jnp.full((V), 0.5)) #0.8
+                              ) 
+        with C_plate:
+            # delta = numpyro.sample('delta', 
+            #                     dist.Normal(jnp.full((n_cov,1, 1, V), 3.0) ,
+            #                                 jnp.full((n_cov,1, 1, V), 0.8))
+            #                   ) 
+            delta = numpyro.sample('delta', 
+                                dist.Normal(jnp.full((n_cov,1, 1, V), delta_v) ,
+                                            jnp.full((n_cov,1, 1, V), 0.1)) # 0.25
+                              ) 
+
+         
+        locs = numpyro.sample('locs', dist.Normal(jnp.full((V,), priors_hyperparameters['mu0_b']) ,
+                                               jnp.full((V,), 1))
+                          )
+        
+        scale1 = numpyro.sample('scale1', dist.Uniform(jnp.full((V,), priors_hyperparameters['lowerBound_scale1']),
+                                                    jnp.full((V,), 0.5))
+                            )
+        
+        sigma_v = numpyro.sample('sigma_v',
+                              dist.Exponential(jnp.full((V,), 
+                                                        priors_hyperparameters['rate0_sigma_v'])))
+        
+        b_lambda = numpyro.sample('b_lambda',
+                                dist.Uniform(jnp.full((V,), -2.0),
+                                             jnp.full((V,),  2.0)))
+        b_lambda2 = numpyro.sample('b_lambda2',
+                                dist.Uniform(jnp.full((V,), -2.0),
+                                             jnp.full((V,),  2.0)))
+        
+
+    delta_ = jnp.sum(jnp.sum(delta * D_matrix_ones_unsqueeze, axis=-4), axis=-2)
+
+
+    
+    with S_plate:
+        with V_plate:
+            weights = numpyro.sample('weights',
+                                  dist.Dirichlet(0.5 * jnp.ones(K)))
+            batch_error = numpyro.sample('error', dist.Uniform(jnp.full((S,V), -flex_mean),
+                                                    jnp.full((S,V), flex_mean)))
+    
+            batch_term1 = b_lambda*b_gamma
+            batch_term2 = b_lambda2*b_gamma2
+
+            batch_term = batch_term1 + (1.0 * batch_term2) + batch_error
             
     with (S_plate, V_plate, D_plate):
         with handlers.mask(mask = mask):
--- uMAIA/normalization/_normalize.py
+++ uMAIA/normalization/_normalize.py
@@ -6,7 +6,7 @@ from numpyro import handlers
 from numpyro.infer import SVI, TraceEnum_ELBO, init_to_value
 from numpyro.infer.autoguide import AutoDelta
 
-from ._model import model
+from ._model import model, model_rank2
 from ._initialize import initialize
 
 
@@ -21,8 +21,9 @@ def normalize(data,
               covariate_vector=None,
               D_matrix_ones=None,
               flex_mean=0.5,
-              seed=42):
-
+              delta_v_dist='gaussian',
+              seed=42,
+              rank=1):
 
     N, S, V = data.shape
     # initialise via GMM
@@ -51,8 +52,7 @@ def normalize(data,
         mask = mask[idx,:,:]
     
     
-    priors_hyperparameters = init_state["prior_hyperparams"]
-    init_values = init_state["init_values"]
+    
     
     
     if optimizer == None:
@@ -61,22 +61,44 @@ def normalize(data,
     if loss == None:
         loss = TraceEnum_ELBO(max_plate_nesting=4)
     
-    
-    global_model = handlers.block(
-        handlers.seed(model, random.PRNGKey(seed)),
-        hide_fn=lambda site: site["name"]
-        not in ['weights', 'locs', 'scale1', 'sigma_v', 'delta',
-                'b_lambda', 'b_gamma', 'sigma_s', 'error',
-                's_plate', 'v_plate', 'c_plate', 'd_plate'],
-    )
-    
+    if rank==1:
+        print('using rank 1')
+        global_model = handlers.block(
+            handlers.seed(model, random.PRNGKey(seed)),
+            hide_fn=lambda site: site["name"]
+            not in ['weights', 'locs', 'scale1', 'sigma_v', 'delta',
+                    'b_lambda', 'b_gamma', 'sigma_s', 'error',
+                    's_plate', 'v_plate', 'c_plate', 'd_plate'],
+        )
+    else:
+        print('line 75: using rank 2')
+        global_model = handlers.block(
+            handlers.seed(model_rank2, random.PRNGKey(seed)),
+            hide_fn=lambda site: site["name"]
+            not in ['weights', 'locs', 'scale1', 'sigma_v', 'delta',
+                    'b_lambda','b_lambda2', 'b_gamma','b_gamma2', 'sigma_s', 'error',
+                    's_plate', 'v_plate', 'c_plate', 'd_plate'],
+        )
+        # add init values
+        init_state['init_values']['b_lambda2'] =init_state['init_values']['b_lambda'].copy() * 1.1
+        init_state['init_values']['b_gamma2'] =init_state['init_values']['b_gamma'].copy() * 1.1
+
+    # set priors and initial states
+    priors_hyperparameters = init_state["prior_hyperparams"]
+    init_values = init_state["init_values"]
+
+    # set the guide
     global_guide = AutoDelta(global_model,
                              init_loc_fn=init_to_value(values=init_values))
     
-    global_svi = SVI(model, global_guide, optim=optimizer, loss=loss)
+    if rank==1:
+        global_svi = SVI(model, global_guide, optim=optimizer, loss=loss)
+    elif rank==2:
+        print('line 94: using rank 2')
+        global_svi = SVI(model_rank2, global_guide, optim=optimizer, loss=loss)
     
     svi_result = global_svi.run(
-            random.PRNGKey(0), num_steps, data, mask, covariate_vector=covariate_vector, D_matrix_ones=D_matrix_ones, priors_hyperparameters=priors_hyperparameters, flex_mean=flex_mean)
+            random.PRNGKey(0), num_steps, data, mask, covariate_vector=covariate_vector, D_matrix_ones=D_matrix_ones, priors_hyperparameters=priors_hyperparameters, flex_mean=flex_mean, delta_v_dist=delta_v_dist)
     
     
     # D_matrix
